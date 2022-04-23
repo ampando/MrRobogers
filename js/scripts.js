aa771950d4dfae955c02bd28ed9267dc6b576e2c
Adrienne Matosich <amatosich@gmail.com>
2022-04-23 15:46:34 -0700

    function gives expected output for all numbers that include the digits 1,2,3.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 //Business Logic 
 function beep(n) {
-  let num = parseInt(n)  // 12 //converts sting input from user to integer for looping. 
+  let num = parseInt(n)  //converts sting input from user to integer for looping. 
   let beepArray = []; //new empty array for new return
   for(i = 0; i <= num; i++) { // 1 //looping through integer variable.
-    if ([i].includes(3)) { //[i] represents integer being iterated through, checks if includes 3
+    if ([i].toString().includes(3)) { //[i] represents integer being iterated through, checks if includes 3
       beepArray.push("won't you be neighbor?") //if condition met, push phrase to beepArray
-    } else if ([i].includes(2)) {
+    } else if ([i].toString().includes(2)) {
       beepArray.push("boop!")
-    } else if ([i].includes(1)) {
+    } else if ([i].toString().includes(1)) { //turn integer to a string so the .includes() method can recognize double, triple and quad digit numbers as individual numbers ex: "143" reads as "1", "4", "3"
       beepArray.push("beep!")
     } else {
       beepArray.push(i)
