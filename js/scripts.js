ce1ca610540fb9d16edd7e69232280d4e02e601d
Adrienne Matosich <amatosich@gmail.com>
2022-04-23 14:29:15 -0700

    changed input of integer 3 to won't you be my neighbor.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 //Business Logic 
 function beep(n) {
   let num = parseInt(n)
-  console.log(num)
   let beepArray = [];
   for(i = 0; i <= num; i++) {
+    if ([i].includes(3)) {
+      beepArray.push("won't you be neighbor?")
+      console.log(beepArray)
+    }  
     console.log(i)
   }
   
@@ -15,3 +18,4 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
   }
+}
