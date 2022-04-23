df32bd6855af3501ad390eb2acde0ccacfd1cbc4
Adrienne Matosich <amatosich@gmail.com>
2022-04-23 16:51:12 -0700

    moved jquery above javascript in html.

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,13 @@ function beep(n) {
   let beepArray = []; //new empty array for new return
   for(i = 0; i <= num; i++) { // 1 //looping through integer variable.
     if ([i].toString().includes(3)) { //[i] represents integer being iterated through, checks if includes 3
-      beepArray.push("won't you be neighbor?") //if condition met, push phrase to beepArray
+      beepArray.push("won't you be neighbor? ") //if condition met, push phrase to beepArray
     } else if ([i].toString().includes(2)) {
-      beepArray.push("boop!")
+      beepArray.push("boop! ")
     } else if ([i].toString().includes(1)) { //turn integer to a string so the .includes() method can recognize double, triple and quad digit numbers as individual numbers ex: "143" reads as "1", "4", "3"
-      beepArray.push("beep!")
+      beepArray.push("beep! ")
     } else {
-      beepArray.push(i)
+      beepArray.push(i + " ")
     }
       
   }
