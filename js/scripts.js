f746dc32d4649d58062785c4d27270be7a7aec70
Adrienne Matosich <amatosich@gmail.com>
2022-04-23 13:39:27 -0700

    fix the size of input box.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
+//UI Logic 
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+  }
+
+
+
+
+
+//Business Logic 
+
 var input = "1-1000"
 var output = "";
 
 for(var ones = 0; ones < 1000; ones ++) {
-  
-}
+{
