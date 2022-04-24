f566ad982e20213dcba3306d569b53ad4f05e12a
Adrienne Matosich <amatosich@gmail.com>
2022-04-23 23:39:24 -0700

    delete comments in js file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 //Business Logic 
 function beep(n) {
-  let num = parseInt(n)  //converts sting input from user to integer for looping. 
-  let beepArray = []; //new empty array for new return
-  for(i = 0; i <= num; i++) { // 1 //looping through integer variable.
-    if ([i].toString().includes(3)) { //[i] represents integer being iterated through, checks if includes 3
-      beepArray.push("won't you be neighbor? ") //if condition met, push phrase to beepArray
+  let num = parseInt(n)  
+  let beepArray = []; 
+  for(i = 0; i <= num; i++) { 
+    if ([i].toString().includes(3)) { 
+      beepArray.push("won't you be neighbor? ") 
     } else if ([i].toString().includes(2)) {
       beepArray.push("boop! ")
-    } else if ([i].toString().includes(1)) { //turn integer to a string so the .includes() method can recognize double, triple and quad digit numbers as individual numbers ex: "143" reads as "1", "4", "3"
+    } else if ([i].toString().includes(1)) { 
       beepArray.push("beep! ")
     } else {
       beepArray.push(i + " ")
